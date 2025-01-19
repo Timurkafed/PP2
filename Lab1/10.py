10ffb2202ea38e492018600bec4078c508499386
Timurkafed <federror.tim@gmail.com>
2025-01-19 23:30:21 +0500

    Python strings

---
+++ Lab1/10.py
@@ -0,0 +1,18 @@
+txt = "The best things in life are free!"
+if "expensive" not in txt:
+  print("No, 'expensive' is NOT present.")
+#----------------------
+b = "Hello, World!"
+print(b[-5:-2])
+#----------------------
+a = "Hello"
+b = "World"
+c = a + " " + b
+print(c)
+#----------------------
+price = 59
+txt = f"The price is {price} dollars"
+print(txt)
+#----------------------
+txt = "We are the so-called \"Vikings\" from the north."
+#----------------------
